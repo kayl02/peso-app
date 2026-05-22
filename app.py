c16b350a0f744902432ac6df594fe0e2c51f5e17
kayl02 <justinekyle.go123@gmail.com>
2026-05-23 03:21:05 +0800

    update database name

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ DB_CONFIG = {
     'host': os.environ.get('MYSQLHOST', 'localhost'),
     'user': os.environ.get('MYSQLUSER', 'root'),
     'password': os.environ.get('MYSQLPASSWORD', ''),
-    'database': os.environ.get('MYSQLDATABASE', 'peso_db'),
+    'database': os.environ.get('MYSQLDATABASE', 'railway'),
     'port': int(os.environ.get('MYSQLPORT', 3306))
 }
 
